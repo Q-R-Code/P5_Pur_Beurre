e0858b653d9f6e60120cf1a571ccc94ef660d9d8
Quentin RUFFIER <qruffier@gmail.com>
2021-01-27 17:05:04 +0100

    test sur l'ajout automatique à la db lors du lancement : OKAY ! 2

--- fill_db.py
+++
@@ -1,8 +0,0 @@
-from Pur_Beurre_P5.off_categories import Categories_request
-from create_db import Categories as Cat, db
-
-categories_request = Categories_request().get_cat()
-
-found_cat = Cat.query.first()
-
-print(type(categories_request))
