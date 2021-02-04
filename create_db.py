7cca248f6f2351194a4e35c15ec2720b799ae682
Quentin RUFFIER <qruffier@gmail.com>
2021-02-04 16:18:21 +0100

    changement affichage recherche, reflexion sur les substitus

--- create_db.py
+++ create_db.py
@@ -20,15 +20,15 @@ class Products(db.Model):
     barcode = db.Column("barcode", db.String(500))
     url = db.Column("url", db.String(500))
     nutrition_grade = db.Column("nutrition_grade", db.String(500))
-    id_categorie = db.Column("id_categorie", db.Integer)
 
-    def __init__(self, id, name, barcode, url, nutrition_grade, id_categorie):
+
+    def __init__(self, id, name, barcode, url, nutrition_grade):
         self.id = id
         self.name = name
         self.barcode = barcode
         self.url = url
         self.nutrition_grade = nutrition_grade
-        self.id_categorie = id_categorie
+
 
 def create_tables():
     db.drop_all()
